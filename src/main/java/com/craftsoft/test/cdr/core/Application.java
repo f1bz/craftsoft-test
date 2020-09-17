aec0536b9db0aa43423b2f5cf188c83a4a8b641b
f1bz <fibbyua@gmail.com>
2020-09-17 15:45:15 +0300

    Init commit

---
+++ src/main/java/com/craftsoft/test/cdr/core/Application.java
@@ -0,0 +1,17 @@
+package com.craftsoft.test.cdr.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Andrew Ruban
+ * @since 17.09.2020
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/craftsoft/test/cdr/core/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craftsoft.test.cdr.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
