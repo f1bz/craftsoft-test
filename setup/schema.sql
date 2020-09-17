c35e2d812ab0121fcf386e92877b63906e853eb4
f1bz <fibbyua@gmail.com>
2020-09-17 16:31:37 +0300

    Add database schema

---
+++ setup/schema.sql
@@ -0,0 +1,26 @@
+create table call_detail_records
+(
+    id uuid not null,
+    account varchar(20) not null,
+    destination varchar(20) not null,
+    start_datetime timestamp not null,
+    end_datetime timestamp not null,
+    status varchar(20) not null,
+    cost_per_minute numeric not null
+);
+
+comment on table call_detail_records is 'This is a main table containing all info about call records ';
+
+create index call_detail_records_account_index
+    on call_detail_records (account);
+
+create index call_detail_records_destination_index
+    on call_detail_records (destination);
+
+create unique index call_detail_records_id_uindex
+    on call_detail_records (id);
+
+alter table call_detail_records
+    add constraint call_detail_records_pk
+        primary key (id);
+
